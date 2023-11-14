84a666f881a7697c96db5e6710cc8bf7fd2f0c6a
Oz Soffy <ozsoffy@gmail.com>
2023-11-14 15:18:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Update the numbers with your actual data
+document.getElementById('deaths').innerText = '1400';
+document.getElementById('injuries').innerText = '1000';
+document.getElementById('towns').innerText = '12';
